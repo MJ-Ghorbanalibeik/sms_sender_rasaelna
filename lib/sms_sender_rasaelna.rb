c43483de5237c9c406f219b31991c7c26a007e50
MJ <mojtaba.ghorbanalibeik@gmail.com>
2014-04-23 01:57:53 +0430

    debuging code removed

--- lib/sms_sender_rasaelna.rb
+++ lib/sms_sender_rasaelna.rb
@@ -3,7 +3,6 @@ module SmsSenderRasaelna
   require "uri"
 
   def self.send_sms(user_name, user_password, numbers, user_sender, message)
-    puts "==================== Enter send_sms method ===================="
     uri = URI.parse("http://rasaelna.com/sms/gw/")
     # According to documentation, message should be ASCII windows-1256 charset encoded
     message.force_encoding("ISO-8859-1")
@@ -15,16 +14,10 @@ module SmsSenderRasaelna
       "msg" => message,
       "By" => "API"
     }
-    puts "==================== HTTP1 ===================="
-    http = Net::HTTP.new(uri.host, uri.port) 
-    puts "==================== HTTP2 ===================="
-    request = Net::HTTP::Get.new(uri.path) 
-    puts "==================== HTTP3 ===================="
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.path)
     request.set_form_data(url_params)
-    puts "==================== HTTP4 ===================="
-    request = Net::HTTP::Get.new(uri.path + '?' + request.body) 
-    puts "==================== HTTP5 ===================="
+    request = Net::HTTP::Get.new(uri.path + '?' + request.body)
     response = http.request(request)
-    puts "==================== HTTP6 ===================="
   end
 end
